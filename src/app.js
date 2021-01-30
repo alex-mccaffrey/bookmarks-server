aaec31ff145820cd824bc64b7439f8e82efb053f
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-01-29 19:47:56 -0700

    fixed validation bearer token

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ app.use(express.json());
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
 
-  if (!authToken || authToken !== apiToken) {
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' })
   }
