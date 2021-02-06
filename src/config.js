429cabba0886b4ab5f684080daa8540c71ecfbfa
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-06 07:32:32 -0700

    added post delete tests

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+  API_TOKEN: process.env.API_TOKEN,
   DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
